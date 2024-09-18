49c3a7156c9ea21ddbdbac98adb3791cef02f732
NanthapreechaSupama <nanthprichas@gmail.com>
2024-09-18 15:07:58 +0700

    ANGULAR-WS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    // BrowserModule,
+    // BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-ws';
+  constructor(private http:HttpClient){
+    // this.getLandmark();
+    // console.log('Start');
+    // let url = 'http://localhost/webapi/landmark';
+    // let obs = this.http.get(url).subscribe((data:any) => {
+    //   console.log(data[0]);
+    //   console.log('Complete');
+    // });
+    // console.log('Continue');
+    // setTimeout(() =>{
+    //   obs.unsubscribe();
+    //   console.log("Unsubscribied");
+    // }, 2500);
+  }
+  async getLandmark(){
+    console.log('Start');
+    let url = 'http://localhost/webapi/landmark';
+    let data:any = await lastValueFrom(this.http.get(url));
+    console.log('Complete');
+    console.log(data[0]);
+
+    console.log('Continue');
+  }
 }
